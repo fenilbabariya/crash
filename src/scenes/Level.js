425dc91a08b002ae872170f374a7fa9a1dd81b49
fenilbabariya <fenilbabariya410@gmail.com>
2023-05-17 23:27:47 +0530

    game UI and timer of next round

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -24,8 +24,59 @@ class Level extends Phaser.Scene {
 		crashBg.scaleY = 1.7;
 		container_background.add(crashBg);
 
+		// graph_x
+		const graph_x = this.add.rectangle(470, 293, 950, 5);
+		graph_x.setOrigin(1, 0.5);
+		graph_x.isFilled = true;
+		graph_x.fillColor = 5000268;
+		container_background.add(graph_x);
+
+		// graph_y
+		const graph_y = this.add.rectangle(470, 293, 5, 610);
+		graph_y.setOrigin(1, 1);
+		graph_y.isFilled = true;
+		graph_y.fillColor = 5000268;
+		container_background.add(graph_y);
+
+		// rocket
+		const rocket = this.add.sprite(250, 1098, "rocket_h", 0);
+
+		// container_timer
+		const container_timer = this.add.container(729, 1080);
+
+		// text_1
+		const text_1 = this.add.text(0, 0, "", {});
+		text_1.setOrigin(0.5, 0.5);
+		text_1.text = "Next round in";
+		text_1.setStyle({ "align": "center", "color": "#000", "fontFamily": "Verdana", "fontSize": "40px", "fontStyle": "bold" });
+		container_timer.add(text_1);
+
+		// txt_game_timer
+		const txt_game_timer = this.add.text(0, 50, "", {});
+		txt_game_timer.setOrigin(0.5, 0.5);
+		txt_game_timer.text = "0";
+		txt_game_timer.setStyle({ "align": "center", "color": "#000", "fontFamily": "Verdana", "fontSize": "40px", "fontStyle": "bold" });
+		container_timer.add(txt_game_timer);
+
+		// txt_amount_x
+		const txt_amount_x = this.add.text(540, 740, "", {});
+		txt_amount_x.setOrigin(0.5, 0.5);
+		txt_amount_x.text = "5X";
+		txt_amount_x.setStyle({ "align": "center", "color": "#00f", "fontFamily": "Verdana", "fontSize": "40px", "fontStyle": "bold" });
+
+		this.rocket = rocket;
+		this.txt_game_timer = txt_game_timer;
+		this.txt_amount_x = txt_amount_x;
+
 		this.events.emit("scene-awake");
 	}
+
+	/** @type {Phaser.GameObjects.Sprite} */
+	rocket;
+	/** @type {Phaser.GameObjects.Text} */
+	txt_game_timer;
+	/** @type {Phaser.GameObjects.Text} */
+	txt_amount_x;
 
 	/* START-USER-CODE */
 
@@ -33,7 +84,25 @@ class Level extends Phaser.Scene {
 
   create() {
     this.editorCreate();
-	
+    this.oGameManager = new GameManager();
+    this.rocket.anims.play("rocket-h");
+    this.setNextGameTimer(this.oGameManager.nNextRoundTime);
+  }
+
+  // set the next round game timer
+  setNextGameTimer(nNextGameTime) {
+    let countdown = setInterval(() => {
+      let minutes = Math.floor(nNextGameTime / 60000);
+      let seconds = Math.floor((nNextGameTime % 60000) / 1000);
+      this.txt_game_timer.setText(
+        `${minutes}.${seconds < 10 ? "0" : ""}${seconds}`
+      );
+      if (nNextGameTime <= 0) {
+        clearInterval(countdown);
+      } else {
+        nNextGameTime -= 1000;
+      }
+    }, 500);
   }
 
   /* END-USER-CODE */
---
+++ src/scripts/GameManager.js
@@ -0,0 +1,5 @@
+class GameManager {
+  constructor() {
+    this.nNextRoundTime = 10 * 1000; // time in seconds
+  }
+}
